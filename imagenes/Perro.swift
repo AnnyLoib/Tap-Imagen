9b429653719610a2703617b2087c1e572c1a3a37
Maestro <Maestro@andres.local>
2018-08-28 18:46:30 -0700

    Tap entre imagenes

---
+++ imagenes/Perro.swift
@@ -0,0 +1,16 @@
+//
+//  Perro.swift
+//  imagenes
+//
+//  Created by Maestro on 28/08/18.
+//  Copyright © 2018 Maestro. All rights reserved.
+//
+
+import Foundation
+
+class perro {
+    var nombre : String?
+    var vida : Int?
+    var edad : Int?
+    
+}
--- imagenes/ViewController.swift
+++ imagenes/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblTexto: UILabel!
+    
+    
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    
+    @IBAction func doTapPerro(_ sender: Any) {
+        lblTexto.text = "Guau"
+        if imgSecuencia.isAnimating {
+            imgSecuencia.stopAnimating()
+            lblTexto.text = "Guau"
+        }
+        else {
+            imgSecuencia.startAnimating()
+            lblTexto.text = "Continuar"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imgSecuencia.image = UIImage(named: "Carta2_00000")
+        
+        var imagenes : [UIImage] = []
+        imagenes.append(UIImage(named: "Carta2_00000")!)
+        imagenes.append(UIImage(named: "Carta2_00001")!)
+        imagenes.append(UIImage(named: "Carta2_00002")!)
+        imagenes.append(UIImage(named: "Carta2_00003")!)
+        imagenes.append(UIImage(named: "Carta2_00004")!)
+        imagenes.append(UIImage(named: "Carta2_00005")!)
+        imagenes.append(UIImage(named: "Carta2_00006")!)
+        imagenes.append(UIImage(named: "Carta2_00007")!)
+        
+        imgSecuencia.animationImages = imagenes
+        
+        
+        imgSecuencia.animationDuration = 0.2
+        imgSecuencia.startAnimating()
+
     }
 
     override func didReceiveMemoryWarning() {
